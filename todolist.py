9b8893c42f14731518e4d9a3187c00ab129d0ff4
Josh <richardwithaq@gmail.com>
2023-06-25 04:53:02 -0600

    Bugs and Runtime Errors #1 fixed
    Fixed bugs:
    -Creates todolist.txt if not present
    -Remove unnecessary spaces in todolist.txt
    -Resolved a ValueError when using list in remove

--- todolist.py
+++ todolist.py
@@ -1,8 +1,14 @@
+import os
 running = True
 start = 0
 addorcheck = []
 todolist = []
 remove = 0
+
+if os.path.exists('todolist.txt'):
+    pass
+else:
+    open('todolist.txt', 'x')
 
 with open('todolist.txt', 'r') as file:
     prelist = file.read()
@@ -11,6 +17,7 @@ prelist = prelist.split()
 while running == True:
     while start == 0:
         addorcheck = input('Would you like to add to or check your list? Use "add" or "check" or "remove" or "exit": ').upper()
+#CHECK
         if addorcheck == 'CHECK':
             with open('todolist.txt', 'r') as file:
                 print(file.read())
@@ -28,7 +35,7 @@ while running == True:
         else:
             print('I am sorry. I do not understand, try typing "add" to add items to the list or "check" to check the list or "remove" to delete something from the list or "exit" to leave. ')
 
-
+#ADD
     while start == 1:
         print('Please use a comma "," after every item')
         todolist = input('''What would you like to add to your list
@@ -36,13 +43,14 @@ while running == True:
 ''')
         todolist = todolist.split(',')
         todolist = prelist + todolist
+        todolist = [item.strip() for item in todolist]
         with open('todolist.txt', 'w') as file:
             for item in todolist:
                 file.write(item + '\n')
         start = 0
         break
 
-
+#REMOVE
     while start == 2:
         while start == 2:
             with open('todolist.txt', 'r') as file:
@@ -50,13 +58,21 @@ while running == True:
 
             for index, word in enumerate(savedlist, start = 1):
                 print(f'{index}. {word.strip()}')
-            try:
-                remove = input('To remove an item type the number next to the item you wish to remove: ')
-                remove = int(remove)
-                savedlist.pop(remove - 1)
+            remove = input('To remove an item type the number next to the item you wish to remove and split numbers with a comma: ')
+            remove = remove.replace(' ', '')
+            remove.split(',')
+            remove = [int(num) for num in remove if num.isdigit()]
+
+            if remove:
+                remove = [num - 1 for num in remove if 0 < num <= len(savedlist)]
+                remove.sort(reverse = True)
+                for index in remove:
+                    savedlist.pop(index)
                 break
-            except IndexError:
-                print('No item has that number.')
+            else:
+                print('Try using numbers instead of letters.')
+            #except IndexError:
+               # print('No item has that number.')
     
         with open('todolist.txt', 'w') as file:
             for item in savedlist:
